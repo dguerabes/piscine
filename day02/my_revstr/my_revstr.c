6e90517eb03f7ed50576d756b9406b9bbbeaa2c3
dguerabes <dguerabes@gmail.com>
2020-11-29 22:31:19 +0100

    add my_revstr

---
+++ day02/my_revstr/my_revstr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void my_putchar(char c);
+
+int my_strlen(const char *str)
+{
+    const char *initPointer = str;
+
+    while (*str != '\0')
+        *str++;
+    return (str - initPointer);
+}
+
+char *my_revstr(char *str)
+{
+    int length = my_strlen(str) - 1;
+    int index = 0;
+
+    while (index <= length)
+    {
+        char c = str[index];
+        str[index] = str[length];
+        str[length] = c;
+        index++;
+        length--;
+    }
+    return str;
+}
